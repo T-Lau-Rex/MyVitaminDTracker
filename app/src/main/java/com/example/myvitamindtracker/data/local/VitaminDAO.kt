dbf713382af07e6effed670dc22c71bd02687ed3
laura <la.calle.de.la.piruleta@hotmail.com>
2026-05-05 14:08:57 +0200

    add VitaminDao + fix KSP sourceSets issue

--- app/src/main/java/com/example/myvitamindtracker/data/local/VitaminDAO.kt
+++ app/src/main/java/com/example/myvitamindtracker/data/local/VitaminDAO.kt
@@ -1,4 +1,20 @@
 package com.example.myvitamindtracker.data.local
 
-interface VitaminDAO {
+import androidx.room.*
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface VitaminDao {
+
+    // Inserta un registro
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(record: VitaminEntity)
+
+    // Elimina un registro
+    @Delete
+    suspend fun delete(record: VitaminEntity)
+
+    // Muestra la lista con los registros
+    @Query("SELECT * FROM vitamin_records ORDER BY timestamp DESC")
+    fun getAllRecords(): Flow<List<VitaminEntity>>
 }
