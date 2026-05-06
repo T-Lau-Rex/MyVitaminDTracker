bb97f4fdd6d0c1d4bbc3ed3fcabd9431430f6574
laura <la.calle.de.la.piruleta@hotmail.com>
2026-05-06 15:41:28 +0200

    Added date column with unique Index to avoid duplicates

--- app/src/main/java/com/example/myvitamindtracker/data/local/VitaminEntity.kt
+++ app/src/main/java/com/example/myvitamindtracker/data/local/VitaminEntity.kt
@@ -1,11 +1,13 @@
 package com.example.myvitamindtracker.data.local
 
 import androidx.room.Entity
+import androidx.room.Index
 import androidx.room.PrimaryKey
 
-@Entity(tableName = "vitamin_records")
+@Entity(tableName = "vitamin_records", indices = [Index(value = ["date"], unique = true)])
 data class VitaminEntity(
     @PrimaryKey(autoGenerate = true)
     val id: Int = 0,
-    val timestamp: Long
+    val timestamp: Long,
+    val date: String // YYYY-MM-DD format. To avoid duplicates records
 )
